5d324e7ec150d6cef14769d215a90a18f6541c20
Alec Gunny <alec.gunny@gmail.com>
2021-08-27 16:58:05 +0000

    getting through presentation

--- projects/slideshow/utils.py
+++ projects/slideshow/utils.py
@@ -2,17 +2,19 @@ import re
 import subprocess
 import time
 from threading import Event, Thread
-from typing import Optional, Sequence
+from typing import Callable, Sequence, Union
 
 import numpy as np
 import torch
 from rich.progress import BarColumn, Progress, TimeElapsedColumn
 
 
-class GPUUtilProgress(Progress):
+class GpuUtilProgress(Progress):
     """Progress bar subclass for measuring GPU utilization"""
 
-    def __init__(self, gpu_ids: Optional[Sequence[int]], *args) -> None:
+    def __init__(
+        self, gpu_ids: Union[int, Sequence[int], None] = None, *args
+    ) -> None:
         if len(args) == 0:
             super().__init__(
                 "[progress.description]{task.description}",
@@ -23,14 +25,19 @@ class GPUUtilProgress(Progress):
         else:
             super().__init__(*args)
 
-        # for each gpu id, make a bar that shows the
-        # utilization and keep track of the associated
-        # task id and most recent utilization for
-        # performing updates
-        for gpu_id in gpu_ids:
-            self.add_task(
-                f"[green]GPU {gpu_id} utilization", total=100, start=False
-            )
+        # if we specified gpu ids to monitor, create
+        # a progress bar for each of them that shows
+        # their utilization level
+        if gpu_ids is not None:
+            # normalize a single integer gpu id
+            # to a sequence for simplicity
+            if isinstance(gpu_ids, int):
+                gpu_ids = [gpu_ids]
+
+            for gpu_id in gpu_ids:
+                self.add_task(
+                    f"[green]GPU {gpu_id} utilization", total=100, start=False
+                )
 
         # use an event to stop the internal loop
         self._stop_event, self._gpu_monitor = None, None
@@ -42,13 +49,18 @@ class GPUUtilProgress(Progress):
         return {
             task_id: task
             for task_id, task in self._tasks.items()
-            if re.fullmatch("[green]GPU [0-9] utilization", task.description)
+            if re.fullmatch("\[green\]GPU [0-9] utilization", task.description)
         }
 
     def __enter__(self):
         # if we have any GPUs to monitor, launch a thread
         # to monitor them with a stop condition via an Event
-        if len(self.gpu_tasks) > 0:
+        gpu_tasks = self.gpu_tasks
+        if len(gpu_tasks) > 0:
+            # start the gpu tasks first
+            for task_id in gpu_tasks:
+                self.start_task(task_id)
+
             self._stop_event = Event()
             self._gpu_monitor = Thread(target=self.monitor_utilization)
             self._gpu_monitor.start()
@@ -88,7 +100,7 @@ class GPUUtilProgress(Progress):
             # parse information for each one of our GPUs
             for task_id, task in tasks.items():
                 # get the GPU id from the task description
-                gpu_id = int(re.find("[0-9]", task.description).group(0))
+                gpu_id = int(re.search("[0-9]", task.description).group(0))
 
                 # use this index to grab the corresponding utilization
                 # and update the corresponding task with the delta
@@ -132,11 +144,9 @@ def do_some_inference(
     device_index: int = 0,
 ) -> np.ndarray:
     """Do inference on the array `X` using the provided model
-
     Iterates through an array of data in chunks and
     performance model inference on it, yielding the model
     output at each iteration.
-
     Args:
         model:
             The torch module to use for inference
@@ -147,7 +157,6 @@ def do_some_inference(
             The size of chunks to iterate through X in
         device_index:
             The GPU index to use for inference
-
     Returns:
         The model output for each input batch
     """
@@ -165,15 +174,17 @@ def do_some_inference(
 
 def parallel_inference_task(
     i: int,
-    jobs_per_gpu: int,
     X: np.ndarray,
+    model: torch.nn.Module,
+    model_args: tuple,
+    inference_fn: Callable,
     q: torch.multiprocessing.Queue,
-    starter: torch.multiprocessing.Value,
+    synchronizer: torch.multiprocessing.Value,
+    jobs_per_gpu,
     num_gpus: int = 1,
     weights_file: str = "model.pt",
 ) -> None:
     """Perform inference on a subset of an input array
-
     Iterate through a subset of an array of data in
     chunks and perform model inference using an MLP
     with weights stored in the specified file. The
@@ -181,22 +192,21 @@ def parallel_inference_task(
     as the GPU assigned for inference is specified by
     the index `i` and the total number of jobs
     `jobs_per_gpu * num_gpus`.
-
     Args:
         i:
             The job index for this inference process
-        jobs_per_gpu:
-            The number of jobs to execute per gpu
         X:
             The array containing _all_ data on which
             inference is desired
         q:
             The queue in which to place inference outputs
-        starter:
+        synchronizer:
             A counter for synchronizing model inference
             across all the jobs, waiting until all
             models are created and have weights loaded
             into memory
+        jobs_per_gpu:
+            The number of jobs to execute per gpu
         num_gpus:
             The number of GPUs to leverage for inference
         weights_file:
@@ -209,19 +219,67 @@ def parallel_inference_task(
     device_index = i // jobs_per_gpu
 
     # build the model and load in its weights
-    model = MLP(64, [256, 128, 64]).cuda(device_index)
+    model = model(*model_args).cuda(device_index)
     model.load_state_dict(torch.load(weights_file))
 
-    # increment the starter to indicate that
-    # this model is ready
-    starter.value += 1
+    # increment the synchronizer to indicate
+    # that this model is ready
+    synchronizer.value += 1
 
     # wait until _all_ child processes have built
     # their models _and_ the parent process has
     # given the okay to start (hence the +1)
-    while starter.value < (jobs_per_gpu * num_gpus) + 1:
+    while synchronizer.value < (jobs_per_gpu * num_gpus) + 1:
         time.sleep(0.01)
 
     # do inference and put it in the q
-    for y in do_some_inference(model, x, device_index=device_index):
+    for y in inference_fn(model, x, device_index=device_index):
         q.put(y)
+
+        
+class ThrottledDataset(torch.utils.data.IterableDataset):
+    def __init__(self, x, device_index=0):
+        self.x = torch.from_numpy(x).cuda(device_index)
+
+    def __iter__(self):
+        self.it = iter(self.x)
+        return self
+
+    def __next__(self):
+        x = next(self.it)
+        time.sleep(0.001)
+        return x
+
+
+@torch.no_grad()
+def do_some_throttled_inference(model, dataset, batch_size=8, device_index=0):
+    # move the data to the GPU in bulk
+    gpu_dataset = ThrottledDataset(dataset, device_index)
+    for x in torch.utils.data.DataLoader(gpu_dataset, batch_size=batch_size):
+        y = model(x)
+        yield y.cpu().numpy()
+
+        
+class NoiseRemovalModel(torch.nn.Module):
+    def __init__(self, input_size: int, hidden_sizes: Sequence[int]) -> None:
+        super().__init__()
+
+        output_size = input_size
+        self.layers = torch.nn.ModuleList()
+        for size in hidden_sizes:
+            self.layers.append(torch.nn.Linear(input_size, size))
+            self.layers.append(torch.nn.ReLU())
+            input_size = size
+
+        for size in hidden_sizes[-2::-1]:
+            self.layers.append(torch.nn.Linear(input_size, size))
+            self.layers.append(torch.nn.ReLU())
+            input_size = size
+
+        # add a sigmoid output layer
+        self.layers.append(torch.nn.Linear(input_size, output_size))
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        for layer in self.layers:
+            x = layer(x)
+        return x

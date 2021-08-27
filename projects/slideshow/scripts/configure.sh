5583673e068e59da27c20f3a5b61a7804e426b97
Alec Gunny <alec.gunny@gmail.com>
2021-08-26 19:53:49 -0700

    adding docs to configure script

--- projects/slideshow/scripts/configure.sh
+++ projects/slideshow/scripts/configure.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -e
 
 echo '''
+# shortcut function for accessing gcloud secrets
 get_secret() {
     secret=$1
     version=${2:-1}
     gcloud secrets versions access $version --secret=$secret
 }
+
+export -f get_secret
 ''' >> ~/.bashrc
 source ~/.bashrc
 
+# add github ssh key for development
 get_secret github-ssh > ~/.ssh/id_ed25519
 chmod 400 ~/.ssh/id_ed25519
 
+# clone the repo and checkout the slideshow branch
 git clone git@github.com:alecgunny/gw-iaas.git
 cd gw-iaas
 git checkout slideshow
 
+# move to this project and set everything up
 cd projects/slideshow
 poetry install
 poetry run jupyter contrib nbextension install --sys-prefix

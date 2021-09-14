197d7092aed2cf6cbd9180b2a7ed5364d24dc45c
Alec Gunny <alec.gunny@gmail.com>
2021-09-14 13:27:04 -0400

    fixing import in unit test

--- libs/gravswell/gravswell.cloudbreak/tests/unit/test_google.py
+++ libs/gravswell/gravswell.cloudbreak/tests/unit/test_google.py
@@ -2,15 +2,14 @@ import os
 
 import pytest
 
-from gravswell.cloudbreak import google as cb
+from gravswell.cloudbreak.clouds import google as cb
 
 
 def test_cluster_manager(zone="us-central1-f"):
     manager = cb.ClusterManager(zone=zone)
-    print(manager.name)
 
     creds = os.environ.pop("GOOGLE_APPLICATION_CREDENTIALS")
     with pytest.raises(ValueError):
-        manager = cb.ClusterManager(zone=zone)
+        manager = cb.ClusterManager(zone=zone)  # noqa
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds

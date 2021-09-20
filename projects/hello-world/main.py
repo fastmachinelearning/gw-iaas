4904083b8b5cb315ce0c1f402320d114d458f998
Alec Gunny <alec.gunny@gmail.com>
2021-09-20 10:06:15 -0400

    fixing hermes imports and adding command line parsing on hello-world

--- projects/hello-world/main.py
+++ projects/hello-world/main.py
@@ -1,22 +1,26 @@
 import argparse
+import inspect
 import logging
+import re
 import time
 from queue import Queue
 from threading import Event
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 from google.cloud import container_v1 as container
+from hermes import quiver as qv
+from hermes.cloudbreak.clouds import google as cb
 from rich.progress import Progress
 from torchvision.models import resnet18
 from tritonclient import grpc as triton
 
-from gravswell import quiver as qv
-from gravswell.cloudbreak.clouds import google as cb
-
 
 def export(
-    model_repository_bucket: str, model_name: str, max_batch_size: int = 16
+    model_repository_bucket: str,
+    model_name: str,
+    max_batch_size: int = 16,
+    instances_per_gpu: int = 4,
 ) -> qv.ModelRepository:
     """Export a ResNet model to a Google Cloud Storage model repository
 
@@ -33,6 +37,9 @@ def export(
         max_batch_size:
             The maximum batch size which will be passed to this
             model at inference time
+        instances_per_gpu:
+            Number of concurrent inference execution instances
+            to host per GPU
     Returns:
         The `ModelRepository` object representing the actual
         repository location
@@ -50,7 +57,7 @@ def export(
 
     # set some config parameters
     model.config.max_batch_size = max_batch_size
-    model.config.add_instance_group(count=4)
+    model.config.add_instance_group(count=instances_per_gpu)
 
     # export the version of this model corresponding to this
     # particular set of (random) weights. Specify the shape
@@ -76,8 +83,45 @@ def build_cluster(
     gpu_type: str = "t4",
     credentials: Optional[str] = None,
 ) -> cb.Cluster:
+    """Start a GKE cluster and add a GPU node pool to it
+
+    Start a GKE cluster using the specified credentials.
+    If `credentials` is left as `None`, the path to a
+    service account JSON will be looked for using the
+    environment variable `GOOGLE_APPLICATION_CREDENTIALS`.
+
+    Args:
+        cluster_name:
+            The name to assign to the new cluster
+        cluster_zone:
+            The region in which to build the cluster
+        num_nodes:
+            The number of GPU-enabled nodes to attach to
+            the cluster once it's started
+        gpus_per_node:
+            The number of GPUs to attach to each node
+            created in the node pool
+        vcpus_per_node:
+            The number of VCPUs to attach to each node
+            created in the node pool
+        gpu_type:
+            The type of GPU to use for inference
+        credentials:
+            Either a string a specifying a path to a user-managed
+            Google Cloud service account key, or `None`, in which
+            case such a string should be attached to the
+            environment variable `GOOGLE_APPLICATION_CREDENTIALS`
+    Returns:
+        The `Cluster` object representing the new cluster
+    """
+
+    # instantiate a manager with credentials which
+    # we can use to create a new cluster
     manager = cb.ClusterManager(zone=cluster_zone, credentials=credentials)
 
+    # create a description of the cluster that
+    # we want to create, starting with a vanilla
+    # default node pool for cluster management
     cluster_config = container.Cluster(
         name=cluster_name,
         node_pools=[
@@ -88,10 +132,19 @@ def build_cluster(
             )
         ],
     )
+
+    # create the cluster using the manager and
+    # then wait for it to be ready
     cluster = manager.add(cluster_config)
     cluster.wait_for_ready()
+
+    # once it's ready, deploy a daemon set which
+    # installs and exposes the GPU drivers to
+    # containers on each node
     cluster.deploy_gpu_drivers()
 
+    # describe a GPU-enabled set of nodes
+    # to attach to the cluster for inference
     node_pool_config = container.NodePool(
         name=f"tritonserver-{gpu_type}-pool",
         initial_node_count=num_nodes,
@@ -100,10 +153,12 @@ def build_cluster(
         ),
     )
 
+    # use the cluster to create this node pool and
+    # wait for it to become ready to use
     node_pool = cluster.add(node_pool_config)
     node_pool.wait_for_ready()
-    cluster = manager.resources[cluster_name]
 
+    # return the cluster for deletion later
     return cluster
 
 
@@ -114,6 +169,30 @@ def do_inference(
     model_version: int = 1,
     batch_size: int = 16,
 ) -> np.ndarray:
+    """Perform asynchronous inference on the provided dataset
+
+    Use an inference service hosted at `server_url` to perform
+    inference on some data in batches.
+
+    Args:
+        dataset:
+            The data on which to perform inference, with the
+            0th axis representing individual samples
+        server_url:
+            The URL at which the inference service is
+            awaiting requests
+        model_name:
+            The name of the model on the inference service
+            to load and use for inference
+        model_version:
+            Which version of the specified model to use for inference
+        batch_size:
+            The number of samples over which to execute inference
+    Returns:
+        An array representing the inference outputs for
+        each sample in the dataset, not necessarily in order.
+    """
+
     # instantiate a client that points to the appropriate address
     client = triton.InferenceServerClient(server_url)
     if not client.is_server_live():
@@ -210,24 +289,100 @@ def main(
     num_nodes: int = 1,
     gpus_per_node: int = 4,
     vcpus_per_node: int = 16,
+    instances_per_gpu: int = 4,
     gpu_type: str = "t4",
     batch_size: int = 4,
     num_samples: int = 5008,
     credentials: Optional[str] = None,
 ) -> None:
-    repo = export(model_repository_bucket, model_name, batch_size)
+    """Export a model to an inference service then do inference with it
 
-    cluster = build_cluster(
-        cluster_name,
-        cluster_zone,
-        num_nodes,
-        gpus_per_node,
-        vcpus_per_node,
-        gpu_type,
-        credentials,
+    Instantiates and exports a ResNet 18 model to a cloud-based
+    model repository, starts up an inference service on Kubernetes
+    that points to it, then performs inference by sending requests
+    consisting of random data to the service from your local device.
+    At the end, the cloud storage bucket used to host the repository
+    and the cluster used to perform inference are deleted to ensure
+    that your project stops incurring costs once the script is done.
+
+    Args:
+        model_name:
+            The name to give to the created model
+        model_repository_bucket:
+            The name to give to the cloud storage bucket used to
+            host the model repository. Must be *globablly* unique
+        cluster_name:
+            The name to assign to the cluster used to host the
+            inference service. Must be unique within the GCP
+            project and zone used
+        cluster_zone:
+            The GCP zone to use for inference. Must have the
+            request type of GPUs available. For a reference on
+            which GPUs are available in what zones, see the
+            support matrix [here]
+            (https://cloud.google.com/compute/docs/gpus/gpu-regions-zones)
+        num_nodes:
+            The number of GPU-enable nodes to use for inference
+        gpus_per_node:
+            The number of GPUs to assign to each node used for inference.
+            The maximum allowable value will depend on the type of
+            GPU used.
+        vcpus_per_node:
+            The number of VCPUs to assign to each node used for inference.
+            The minimum allowable value will depend on the value
+            of `gpus_per_node`
+        instances_per_gpu:
+            The number of concurent execution instances of
+            the model to host per GPU
+        gpu_type:
+            The type of GPU to use for inference. Should match the
+            last part of the strings listed [here]
+            (https://cloud.google.com/compute/docs/gpus#introduction).
+            E.g. `"t4"`, `"v100"`, etc.
+        batch_size:
+            The number of dataset samples to include with each
+            inference request.
+        num_samples:
+            The number of inferences to perform
+        credentials:
+            The path to a JSON file containing user-managed
+            service account credentials or `None`, in which case
+            such a path should be contained in the environment
+            variable `GOOGLE_APPLICATION_CREDENTIALS`
+    Returns:
+        The model inference outputs on the random data
+    """
+
+    # instantiate a model and a model repository, then
+    # export the model to that repository
+    repo = export(
+        model_repository_bucket=model_repository_bucket,
+        model_name=model_name,
+        max_batch_size=batch_size,
+        instances_per_gpu=instances_per_gpu,
     )
 
+    # create a cluster and attach some GPU-enabled nodes to it
+    cluster = build_cluster(
+        cluster_name=cluster_name,
+        cluster_zone=cluster_zone,
+        num_nodes=num_nodes,
+        gpus_per_node=gpus_per_node,
+        vcpus_per_node=vcpus_per_node,
+        gpu_type=gpu_type,
+        credentials=credentials,
+    )
+
+    # do everything else in a try-catch that way
+    # if anything goes wrong we make sure to clean
+    # up our resource usage
     try:
+        # deploy the workload described by `tritonserver.yaml`
+        # to the cluster, specifying some of the parameters
+        # as variables dynamically. This workload just runs
+        # the triton server on the specified number of nodes
+        # and exposes them to external requests via a LoadBalancer
+        # service. See the YAML for more info
         deployment, load_balancer = cluster.deploy(
             "tritonserver.yaml",
             name="tritonserver",
@@ -238,36 +393,79 @@ def main(
             tag="20.11",
         )
 
+        # wait for all the inference service instances to
+        # come online before continuing. Use the IP assigned
+        # to the load balancer to make requests
         deployment.wait_for_ready()
         server_url = f"{load_balancer.ip}:8001"
 
+        # instantiate a random dataset in-memory. Your real
+        # dataset may require more exotic schemes to be able
+        # to meet the throughput capacity of the server
         dataset = np.random.randn(num_samples, 3, 224, 224).astype("float32")
+
+        # do some inference on this dataset using the model
+        # we created earlier, which is being hosted by the
+        # inference service at the specified URL
         results = do_inference(
             dataset, server_url, model_name, batch_size=batch_size
         )
     finally:
+        # no matter what happened, clean up all of the
+        # resources we were using to avoid incurring extra costs
         cluster.remove()
         repo.delete()
+
+    # return the inference results for downstream processing
     return results
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description=(
-            "Export a model and serve a model " "using an inference service"
-        )
-    )
-    parser.add_argument(
-        "--model-name",
-        type=str,
-        required=True,
-        help="Name to give the exported model",
-    )
-    parser.add_argument(
-        "--model-repository-bucket",
-        type=str,
-        required=True,
-        help="Name of the GCS bucket to store the model repo",
-    )
+    # build a command line parser programatically from
+    # the documentation and annotations on `main`
+    doc, args = main.__doc__.split("Args:\n")
+    args, _ = args.split("Returns:\n")
 
-    main("my-model", "hermes-hello-world", "hermes-hw-cluster", "us-east4-b")
+    parser = argparse.ArgumentParser(
+        prog="Hermes Hello World",
+        description=doc,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    for name, arg in inspect.signature(main).parameters.items():
+        # first check if the argument has an `Optional` annotation
+        try:
+            if arg.annotation.__origin__ == Union:
+                # if so, use the argument of the annotation
+                # as the type for the argument
+                type_ = arg.annotation.__args__[0]
+        except AttributeError:
+            # otherwise just use the annotation itself as the type
+            type_ = arg.annotation
+
+        # do a regex search to get the help description for the argument
+        doc_re = "(?<={}:\n).+(?=\n{})"
+        try:
+            postfix = " " * 8 + "[a-z]"
+            doc_str = re.search(doc_re.format(name, postfix), args).group(0)
+        except AttributeError:
+            # catch for the last argument, which won't
+            # have another argument after it
+            doc_str = re.search(doc_re.format(name, " " * 4), args).group(0)
+        doc_str = " ".join([i.strip() for i in doc_str.split("\n")])
+
+        # use dashes instead of underscores in arg names
+        # then add the argument, using the default if
+        # there is one, otherwise make it required
+        arg_name = "--" + name.replace("_", "-")
+        if arg.default == inspect._empty:
+            parser.add_argument(
+                arg_name, type=type_, required=True, help=doc_str
+            )
+        else:
+            parser.add_argument(
+                arg_name, type=type_, default=arg.default, help=doc_str
+            )
+
+    # parse the arguments and run `main` with them
+    flags = parser.parse_args()
+    main(**vars(flags))

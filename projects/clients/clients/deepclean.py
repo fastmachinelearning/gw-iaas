ce142178a18fa6b726f9cd3faa991d9e56c86289
Alec Gunny <alec.gunny@gmail.com>
2021-10-19 21:21:22 -0700

    moving utils to separate file

--- projects/clients/clients/deepclean.py
+++ projects/clients/clients/deepclean.py
@@ -1,138 +1,17 @@
-import logging
-import os
 from multiprocessing import Queue
-from queue import Empty
 from typing import Optional, Sequence
 
-import numpy as np
-from gwpy.timeseries import TimeSeries
+from clients.utils import FrameWriter, Preprocessor, logger
 
 from hermes.gwftools import FrameCrawler, FrameLoader, GCSFrameDownloader
-from hermes.gwftools.gwftools import _parse_frame_name
-from hermes.stillwater import InferenceClient, PipelineProcess
+from hermes.stillwater import InferenceClient
 from hermes.typeo import typeo
-
-
-class Preprocessor:
-    def __init__(self, preproc_params):
-        pass
-
-
-class FrameCollector(PipelineProcess):
-    def __init__(
-        self,
-        write_dir: str,
-        channel_name: str,
-        step_size: int,
-        sample_rate: float,
-        strain_q: Queue,
-        preprocessor: Optional[Preprocessor] = None,
-        *args,
-        **kwargs,
-    ) -> None:
-        super().__init__(*args, **kwargs)
-
-        if not os.path.exists(write_dir):
-            os.makedirs(write_dir)
-
-        self.write_dir = write_dir
-        self.channel_name = channel_name
-        self.step_size = step_size
-        self.sample_rate = sample_rate
-        self.strain_q = strain_q
-        self.preprocessor = preprocessor
-
-        self._strains = []
-        self._noises = np.array([])
-        self._covered_idx = np.array([])
-        self._frame_idx = 0
-
-    def get_package(self):
-        # now get the next inferred noise estimate
-        noise_prediction = super().get_package()
-
-        # first see if we have any new strain data
-        # to collect
-        try:
-            if len(self._noises) == 0:
-                fname, strain = self.strain_q.get(True, 10)
-            else:
-                fname, strain = self.strain_q.get(False)
-        except Empty:
-            if len(self._noises) == 0:
-                raise RuntimeError("No strain data after 10 seconds")
-        else:
-            # if we do, add it to our running list of strains
-            self._strains.append((fname, strain))
-
-            # create a blank array to fill out our noise
-            # and idx arrays as we collect more data
-            zeros = np.zeros_like(strain)
-            self._noises = np.append(self._noises, zeros)
-            self._covered_idx = np.append(self._covered_idx, zeros)
-
-        return noise_prediction
-
-    def process(self, package):
-        # grab the noise prediction from the package
-        # slice out the batch and channel dimensions,
-        # which will both just be 1 for this pipeline
-        x = package.x.reshape(-1)
-        if len(x) != self.step_size:
-            raise ValueError(
-                "Noise prediction is of wrong length {}".format(len(x))
-            )
-
-        # use the package request id to figure out where
-        # in the blank noise array we need to insert
-        # this prediction. Subtract the running index
-        # of the total number of samples we've processed so far
-        start = package.request_id * self.step_size - self._frame_idx
-        self._noises[start : start + len(x)] = x
-
-        # update our mask to indicate which parts of our
-        # noise array have had inference performed on them
-        self._covered_idx[start : start + len(x)] = 1
-
-        # if we've completely performed inference on
-        # an entire frame's worth of data, postprocess
-        # the predictions and produce the cleaned estimate
-        if self._covered_idx[: len(self._strains[0][1])].all():
-            # pop out the earliest strain and filename and
-            fname, strain = self._strains.pop(0)
-            fname = os.path.basename(fname)
-            timestamp, _ = _parse_frame_name(fname)
-
-            # remove the data from both the running
-            # noise array and the mask
-            noise, self._noises = np.split(self._noises, [len(strain)])
-            self._covered_idx = self._covered_idx[len(strain) :]
-            self._frame_idx += len(noise)
-
-            # now postprocess the noise channel
-            noise = self.preprocessor.uncenter(noise)
-            noise = self.preprocessor.bandpass(noise)
-
-            # remove the noise from the strain channel and
-            # use it to create a timeseries we can write to .gwf
-            cleaned = strain - noise
-            timeseries = TimeSeries(
-                cleaned,
-                t0=timestamp,
-                sample_rate=self.sample_rate,
-                channel=self.channel_name,
-            )
-
-            # write the file and pass the written filename
-            # to downstream processess
-            write_fname = os.path.join(self.write_dir, fname)
-            timeseries.write(write_fname)
-            super().process(write_fname)
 
 
 @typeo("DeepClean client")
 def main(
     data_dir: str,
+    write_dir: str,
     kernel_length: float,
     stride_length: float,
     sample_rate: float,
@@ -218,10 +97,20 @@ def main(
         name="client",
     )
 
+    writer = FrameWriter(
+        write_dir=write_dir,
+        channel_name=channels[0],
+        step_size=int(stride_length * sample_rate),
+        sample_rate=sample_rate,
+        strain_q=strain_q,
+        preprocessor=preprocessor,
+        name="writer",
+    )
+
     # build a pipeline connecting all the processes
-    with fname_source >> data_loader >> client as pipeline:
+    with fname_source >> data_loader >> client >> writer as pipeline:
         for fname in pipeline:
-            logging.info(f"Processed frame {fname}")
+            logger.info(f"Processed frame {fname}")
 
 
 if __name__ == "__main__":

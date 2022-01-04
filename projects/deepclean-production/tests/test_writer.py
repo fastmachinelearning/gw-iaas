d893e57d954e6707c2e021f8525239a4b3f7f913
Alec Gunny <alec.gunny@gmail.com>
2022-01-04 12:48:07 -0800

    fixing test

--- projects/deepclean-production/tests/test_writer.py
+++ projects/deepclean-production/tests/test_writer.py
@@ -38,17 +38,17 @@ def fnames(write_dir, timestamp):
     return witness_fnames, strain_fnames
 
 
-@pytest.fixture(params=[8])# , 64, 256])
+@pytest.fixture(params=[8])  # , 64, 256])
 def step_size(request):
     return request.param
 
 
-@pytest.fixture(params=[2]) # , 6, 10])
+@pytest.fixture(params=[2])  # , 6, 10])
 def memory(request):
     return request.param
 
 
-@pytest.fixture(params=[40 / 4096]) # , 0.5, 1])
+@pytest.fixture(params=[40 / 4096])  # , 0.5, 1])
 def filter_pad(request):
     return request.param
 
@@ -68,7 +68,7 @@ def test_frame_writer(write_dir, fnames, step_size, memory, filter_pad):
         filter_pad=filter_pad,
         strain_q=strain_q,
         preprocessor=preprocessor,
-        name="writer"
+        name="writer",
     )
     writer.logger = logging.getLogger()
 
@@ -79,38 +79,39 @@ def test_frame_writer(write_dir, fnames, step_size, memory, filter_pad):
     noises = np.split(x, num_steps)
     strains = np.split(x, len(strain_fnames))
 
-    for x in noises:
+    writer.strain_q.put(((witness_fnames[0], strain_fnames[0]), strains[0]))
+
+    pad_samples = int(4096 * filter_pad)
+    for i, x in enumerate(noises):
         writer.in_q.put((x,))
 
-    for (w_fname, s_fname, x) in zip(witness_fnames, strain_fnames, strains):
-        writer.strain_q.put(((w_fname, s_fname), x))
-
-    steps_per_frame = 4096 // step_size
-    for i in range(num_steps):
         noise = writer.get_package()
         package = Mock()
         package.x = noise[0]
         package.request_id = i
-
-        if i < len(strain_fnames):
-            assert len(writer._strains) == (i + 1)
-
         writer.process({"aggregator": package})
-        if i < memory:
-            assert len(writer._noises) == (4096 * (i + 1 + filter_pad))
-        else:
-            assert len(writer._noises) == (4096 * (memory + 1 + filter_pad))
 
+        frame_end = (i + 1) * step_size
+        if (frame_end / 2048) % 2 == 1:
+            idx = (frame_end + 2048) // 4096
+            if idx < len(strains):
+                writer.strain_q.put(
+                    ((witness_fnames[idx], strain_fnames[idx]), strains[idx])
+                )
 
-    for fname in strain_fnames:
-        try:
-            written, _ = writer.out_q.get_nowait()
-        except Empty:
-            raise ValueError
-        assert written == fname
+        if frame_end > pad_samples and (frame_end - pad_samples) % 4096 == 0:
+            try:
+                written, _ = writer.out_q.get_nowait()
+            except Empty:
+                raise ValueError(str(frame_end))
 
-        ts = TimeSeries.read(fname, "dummy")
-        assert (ts.value == 0).all()
+            idx = (frame_end - pad_samples) // 4096 - 1
+            assert written == strain_fnames[idx]
 
-    with pytest.raises(Empty):
-        writer.out_q.get_nowait()
+            ts = TimeSeries.read(written, "dummy")
+            assert (ts.value == 0).all()
+
+            num_remaining_frames = min(memory, idx + 1)
+            assert len(writer._noises) == (
+                num_remaining_frames * 4096 + pad_samples
+            )
